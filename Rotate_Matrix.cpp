e92a53419aed85c991406bbdfe02b92fcb50c97b
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-06-06 19:00:43 +0530

    Update Rotate_Matrix.cpp

--- Rotate_Matrix.cpp
+++ Rotate_Matrix.cpp
@@ -13,3 +13,32 @@ public:
             }
     }
 };
+
+//if matrix is not square
+#include <bits/stdc++.h> 
+
+void rotateMatrix(vector<vector<int>> &mat, int n, int m)
+{
+    int top = 0,down = n-1,left = 0,right = m-1;
+    if(down ==0 and right == 0)return;
+    while(top<down and left<right){
+        int temp = mat[top][top];
+        for(int i = left+1;i<=right;i++){
+            swap(temp,mat[top][i]);
+        }
+        for(int i =top+1;i<=down;i++){
+            swap(temp,mat[i][right]);
+        }
+        for(int i = right-1;i>=left;i--){
+            swap(temp,mat[down][i]);
+        }
+        for(int i = down-1;i>=top;i--){
+            swap(temp,mat[i][left]);
+        }
+        top++;down--;
+        left++;right--;
+    }
+    
+    // Write your code here
+
+}
