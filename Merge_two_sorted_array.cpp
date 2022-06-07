88a8bc994175a9ab51218bf7bd9b25b46b5d7066
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-06-07 09:17:00 +0530

    Create Merge_two_sorted_array.cpp

---
+++ Merge_two_sorted_array.cpp
@@ -0,0 +1,19 @@
+//With extra space
+
+#include <bits/stdc++.h> 
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+    int i = 0,j = 0;
+    vector<int>ans;
+    while(i< m and j < n){
+        if(arr1[i]<arr2[j]){
+                ans.push_back(arr1[i++]);
+        }else if(arr1[i] == arr2[j]){
+             ans.push_back(arr1[i++]);
+            ans.push_back(arr2[j++]);
+        }else ans.push_back(arr2[j++]);
+    }
+    while(i<m) ans.push_back(arr1[i++]);
+    while(j<n) ans.push_back(arr2[j++]);
+    return ans;
+	// Write your code here.
+}
