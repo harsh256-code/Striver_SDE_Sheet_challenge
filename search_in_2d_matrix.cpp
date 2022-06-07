ea49c359280a8e8fe221fc257896e55ba9744344
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-06-07 17:22:18 +0530

    Create search_in_2d_matrix.cpp

---
+++ search_in_2d_matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(),m = matrix[0].size();
+        int low = 0,high = (m*n)-1;
+        while(low<=high){
+            int mid = (low+high)>>1;
+            if(matrix[mid/m][mid%m] == target)return true;
+            else if(matrix[mid/m][mid%m]>target) high= mid-1;
+            else low = mid+1;
+        }
+        return false;
+    }
+};
