544769310bbf0100fe64a0741c755facc1b2ef30
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-06-07 09:56:25 +0530

    Update Merge_two_sorted_array.cpp

--- Merge_two_sorted_array.cpp
+++ Merge_two_sorted_array.cpp
@@ -28,3 +28,37 @@ public:
         }
     }
 };
+
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            int gap = ceil((float)(n+m)/2);
+            while(gap>0){
+                int i = 0;
+                int j = gap;
+                while(j<(n+m)){
+                    if(j<n and arr1[i]>arr1[j]) swap(arr1[i],arr1[j]);
+                    else if(j>=n and i<n and arr1[i]>arr2[j-n]) swap(arr1[i],arr2[j-n]);
+                    else if(j>=n and i>=n and arr2[i-n]> arr2[j-n]) swap(arr2[i-n],arr2[j-n]);
+                    
+                    j++;
+                    i++;
+                }
+                if(gap == 1){
+                    gap = 0;
+                }else {
+                    gap = ceil((float)gap/2);
+                }
+            }
+            // code here 
+        } 
+};
+
