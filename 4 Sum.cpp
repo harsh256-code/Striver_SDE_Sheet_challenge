fe1b03e4cf1d9c7fc9040f346b3157fe16e24844
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-06-09 16:24:51 +0530

    Create 4 Sum.cpp

---
+++ 4 Sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        if(nums.empty()) return ans;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            for(int j =i+1;j<n;j++){
+                int target2 = target - nums[i] - nums[j];
+                int front = j+1,back = n-1;
+                while(front<back){
+                    if(nums[front] + nums[back]<target2) front++;
+                    else if(nums[front] + nums[back]>target2) back--;
+                    else {
+                        vector<int>q(4);
+                        q[0] = nums[i];
+                        q[1] = nums[j];
+                        q[2] = nums[front];
+                        q[3] = nums[back];
+                        ans.push_back(q);
+                        
+                        while(front<back and nums[front] == q[2]) front++;
+                        while(front<back and nums[back] == q[3])back--;
+                        
+                    }
+                }
+                while(j+1<n and nums[j+1] == nums[j]) j++;
+                
+            }
+            while(i+1<n and nums[i+1]==nums[i])i++;
+        }
+        return ans;
+    }
+};
