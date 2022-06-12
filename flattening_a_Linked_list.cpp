92012b1e59fbdf4ca52f4b97bd1c806fff2c1f9f
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-06-12 11:48:50 +0530

    Create flattening_a_Linked_list.cpp

---
+++ flattening_a_Linked_list.cpp
@@ -0,0 +1,144 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+    
+Node *mergerTwoList(Node *head1,Node *head2){
+    Node *res = new Node(0);
+    Node *temp = res;
+    
+    while(head1 and head2){
+        if(head1->data<head2->data){
+            temp->bottom = head1;
+            temp = temp->bottom;
+            head1 = head1->bottom;
+        }else{
+            temp->bottom = head2;
+            temp = temp->bottom;
+            head2 = head2->bottom;
+        }
+    }
+    if(head1) temp->bottom = head1;
+    if(head2) temp->bottom = head2;
+    return res->bottom;
+}    
+Node *flatten(Node *root)
+{
+    if(!root or !root->next) return root;
+    
+    root->next = flatten(root->next);
+    
+    root = mergerTwoList(root,root->next);
+    
+    return root;
+   // Your code here
+}
+
