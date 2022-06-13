6e486349eac818664a4b4d8a88a28b1135e1f469
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-06-13 10:01:00 +0530

    Update Max_conseecutive_Ones.cpp

--- Max_conseecutive_Ones.cpp
+++ Max_conseecutive_Ones.cpp
@@ -11,3 +11,20 @@ public:
         return ans;
     }
 };
+
+//if we allowed to change k zero to one the what is the value of max ones
+int longestSubSeg(vector<int> &arr , int n, int k){
+    int cz= 0;
+    int ans = 0;
+    int j =0;
+    for(int i = 0;i<n;i++){
+        if(arr[i]==0)cz++;
+        while(cz>k){
+            if(arr[j]==0)cz--;
+            j++;
+        }
+        ans = max(ans,(i-j)+1);
+    }
+    return ans;
+    // Write your code here.
+}
