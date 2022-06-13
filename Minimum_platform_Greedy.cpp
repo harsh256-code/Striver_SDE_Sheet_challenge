7c303cfbdf764878b88daa92218adddc2af15e62
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-06-13 16:58:19 +0530

    Update Minimum_platform_Greedy.cpp

--- Minimum_platform_Greedy.cpp
+++ Minimum_platform_Greedy.cpp
@@ -1,14 +1,57 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        int i = 0;
-        for(int j = 1;j<nums.size();j++){
-            if(nums[i]!=nums[j]){
+// { Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+        sort(arr,arr+n);
+        sort(dep,dep+n);
+        int i = 1,j= 0;
+        int ans = 1,count = 1;
+        while(i<n and j<n){
+            if(arr[i]<=dep[j]){
+                count++;
                 i++;
-                nums[i] = nums[j];
-                
+            }else{
+                count--;
+                j++;
             }
+            ans = max(ans,count);
         }
-        return i+1;
+        return ans;
+        
+    	// Your code here
     }
 };
+
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}  // } Driver Code Ends
