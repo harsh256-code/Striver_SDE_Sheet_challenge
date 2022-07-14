aa0608d5f9b12b8b53f368b3e26e90ec933ea71f
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-07-14 10:44:35 +0530

    Create Largest_BST_in_BT.cpp

---
+++ Largest_BST_in_BT.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class NodeValue {
+public:
+    int maxNode, minNode, maxSize;
+    
+    NodeValue(int minNode, int maxNode, int maxSize) {
+        this->maxNode = maxNode;
+        this->minNode = minNode;
+        this->maxSize = maxSize;
+    }
+};
+
+class Solution {
+public:
+    int ans = 0;
+    NodeValue solve(TreeNode *root){
+        if (!root) {
+            return NodeValue(INT_MAX, INT_MIN, 0);
+        }
+        
+        // Get values from left and right subtree of current tree.
+        auto left = solve(root->left);
+        auto right = solve(root->right);
+        
+        // Current node is greater than max in left AND smaller than min in right, it is a BST.
+        if (left.maxNode < root->val && root->val < right.minNode) {
+            ans = max(ans,left.maxSize + right.maxSize + root->val);
+            // It is a BST.
+            return NodeValue(min(root->val, left.minNode), max(root->val, right.maxNode), 
+                            left.maxSize + right.maxSize + root->val);
+        }
+        
+        // Otherwise, return [-inf, inf] so that parent can't be valid BST
+        return NodeValue(INT_MIN, INT_MAX, max(left.maxSize, right.maxSize));
+    }
+    int maxSumBST(TreeNode* root) {
+        solve(root);
+        return ans >0?ans:0;
+    }
+};
