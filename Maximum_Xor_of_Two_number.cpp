2d48158c71a98716bbe8ece7461fb8e953ae2ad3
HARSH VISHWAKARMA <70187887+harsh256-code@users.noreply.github.com>
2022-07-26 21:01:14 +0530

    Create Maximum_Xor_of_Two_number.cpp

---
+++ Maximum_Xor_of_Two_number.cpp
@@ -0,0 +1,60 @@
+class Node{
+    
+    Node *links[2];
+    public:
+    bool containKey(int bit){
+        return (links[bit]!=NULL);
+    }
+    Node *get(int bit){
+        return links[bit];
+    }
+    void put(int bit,Node *node){
+        links[bit] = node;
+    }
+};
+class Trie{
+    private: Node *root;
+    public:
+    Trie(){
+        root = new Node();
+    }
+    
+    void insert(int num){
+        Node *node = root;
+        for(int i =31;i>=0;i--){
+            int bit = (num>>i) & 1;
+            if(!node->containKey(bit)){
+                node->put(bit,new Node());
+            }
+            node = node->get(bit);
+        }
+    }
+    int getMax(int num){
+        Node *node = root;
+        int maxAns=0;
+        for(int i=31;i>=0;i--){
+            int bit = (num>>i) & 1;
+            if(node->containKey(!bit)){
+                maxAns = maxAns | (1<<i);
+                node = node->get(!bit);
+            }else{
+                node = node->get(bit);
+            }
+        }
+        return maxAns;
+    }
+};
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trie;
+        for(auto it:nums){
+            trie.insert(it);
+        }
+        int ans =0;
+        for(auto it:nums){
+            ans = max(ans,trie.getMax(it));
+        }
+        return ans;
+    }
+};
